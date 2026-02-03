55af4e6ffcd07a931106580503dd88316d206132
GianPi89 <oterogianpierre@gmail.com>
2026-02-02 22:41:35 -0500

    feat: Se añadieron datos simulados para los movimientos financieros.

---
+++ fintech-frontend/src/data/movimientosMock.js
@@ -0,0 +1,5 @@
+export const movimientosMock = [
+  { id: 1, fecha: "2025-01-10", descripcion: "Pago servicio", monto: -45000 },
+  { id: 2, fecha: "2025-01-12", descripcion: "Transferencia recibida", monto: 200000 },
+  { id: 3, fecha: "2025-01-15", descripcion: "Compra con tarjeta", monto: -120000 }
+];
